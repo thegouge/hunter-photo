2e6cb9223f2b80add48203c64c06ad04fa1d3827
thegouge <thegouge42@gmail.com>
2021-05-08 21:32:28 -0700

    get profile picture from the backend

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,16 +1,23 @@
 <script>
-	import image from '$lib/images/Sanic.jpg';
+	import axios from 'axios';
 	import '../app.css';
 	import NavLink from '../components/NavLink.svelte';
+	import { API_URL } from '../lib/constants';
 
 	let links = ['Home', 'About', 'Photos', 'Design', 'Twitch'];
 
-	let src = image;
+	let src, alt;
+
+	axios.get(`${API_URL}/profile-picture`).then((response) => {
+		console.log(response);
+		src = `${API_URL}${response.data.image[0].url}`;
+		alt = response.data.image[0].alternativeText;
+	});
 </script>
 
 <nav class="w-1/6 h-screen fixed bg-sidebar text-white p-5">
 	<div class="logo">
-		<img {src} alt="is me!" class="rounded-full border-gray-400 border-2 w-20 mx-auto" />
+		<img {src} {alt} class="rounded-full border-gray-400 border-2 w-20 mx-auto" />
 		<h1 class="text-center text-xl font-semibold mt-2">Hunter Smith</h1>
 	</div>
 	<ul class="">
