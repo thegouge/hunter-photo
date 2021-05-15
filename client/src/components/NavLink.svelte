a58d9e1ab480c94c434cd49018195ec866deca26
thegouge <thegouge42@gmail.com>
2021-05-15 11:18:45 -0700

    make navigation a bit better

--- client/src/components/NavLink.svelte
+++ client/src/components/NavLink.svelte
@@ -1,6 +1,8 @@
 <script>
 	export let link;
 
+	import { goto } from '$app/navigation';
+
 	function calcHref(text) {
 		if (text === 'Home' || text === 'home') return '/';
 		return `/${text.toLowerCase()}`;
@@ -9,7 +11,10 @@
 	$: href = calcHref(link);
 </script>
 
-<li class="p-5 hover:bg-yellow-700 transition duration-200 rounded">
+<li
+	class="p-5 hover:bg-yellow-700 transition duration-200 rounded cursor-pointer"
+	on:click={goto(href)}
+>
 	<!-- <span>{icon}</span> -->
 	<a {href}> {link} </a>
 </li>
