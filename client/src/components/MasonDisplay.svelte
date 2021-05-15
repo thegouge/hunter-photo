6513f013408c0ff1077e1a5259ab79fd2ee63dcf
thegouge <thegouge42@gmail.com>
2021-05-15 14:57:32 -0700

    added a masonry image display component

---
+++ client/src/components/MasonDisplay.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let images;
+	console.log({ images });
+	import Masonry from 'svelte-masonry/Masonry.svelte';
+</script>
+
+<Masonry>
+	{#each images as img (img.id)}
+		<div>
+			<img src={img.src} alt={img.alt} />
+		</div>
+	{:else}
+		<div>There are no photos here!</div>
+	{/each}
+</Masonry>
