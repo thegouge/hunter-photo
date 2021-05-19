1ebb2fba3eb3fbaa015f7a3209770deefcc0db39
thegouge <thegouge42@gmail.com>
2021-05-18 20:40:54 -0700

    actually setting the url for production

--- client/src/lib/constants.js
+++ client/src/lib/constants.js
@@ -1,3 +1,2 @@
-import { dev } from '$app/env'
 
-export const API_URL = import.meta.env[`${dev ? 'VITE_DEV_URL' : 'https://hunter-photo.herokuapp.com/'}`] 
+export const API_URL = import.meta.env[`VITE_DEV_URL`] ?? 'https://hunter-photo.herokuapp.com/'
