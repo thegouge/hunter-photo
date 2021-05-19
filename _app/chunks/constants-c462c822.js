4d165222c8df371564ceab01b90fe4c965b83724
thegouge <thegouge42@gmail.com>
2021-05-18 20:11:02 -0700

    Updates

---
+++ _app/chunks/constants-c462c822.js
@@ -0,0 +1,1 @@
+const o={VITE_SVELTEKIT_AMP:"",VITE_SVELTEKIT_SERVICE_WORKER:"",VITE_DEV_URL:"http://localhost:1337",VITE_PROD_URL:"https://hunter-photo.herokuapp.com",BASE_URL:"/hunter-photo/_app/",MODE:"production",DEV:!1,PROD:!0}["https://hunter-photo.herokuapp.com/"];export{o as A};
