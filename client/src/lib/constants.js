195df428a90f4731e0410102a894f764339c476f
thegouge <thegouge42@gmail.com>
2021-05-18 20:10:24 -0700

    hard-coding the fallback url for prod

--- client/src/lib/constants.js
+++ client/src/lib/constants.js
@@ -1,3 +1,3 @@
 import { dev } from '$app/env'
 
-export const API_URL = import.meta.env[`${dev ? 'VITE_DEV_URL' : 'VITE_PROD_URL'}`] 
+export const API_URL = import.meta.env[`${dev ? 'VITE_DEV_URL' : 'https://hunter-photo.herokuapp.com/'}`] 
