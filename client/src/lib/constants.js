00a16de9599f3d7daee0f10e7ad2f11d19dcd28a
thegouge <thegouge42@gmail.com>
2021-05-19 21:18:15 -0700

    freaking trailing slash

--- client/src/lib/constants.js
+++ client/src/lib/constants.js
@@ -1,3 +1,3 @@
 import { dev } from '$app/env';
 
-export const API_URL = dev ? import.meta.env['VITE_DEV_URL'] : 'https://hunter-photo.herokuapp.com/';
+export const API_URL = dev ? import.meta.env['VITE_DEV_URL'] : 'https://hunter-photo.herokuapp.com';
