cd7ef9c209f619d1816de44dcb0deeea82a1a6f1
thegouge <thegouge42@gmail.com>
2021-05-19 21:02:51 -0700

    trying to get this freaking url righ

--- client/src/lib/constants.js
+++ client/src/lib/constants.js
@@ -1,2 +1,3 @@
+import { dev } from '$app/env';
 
-export const API_URL = import.meta.env[`VITE_DEV_URL`] ?? 'https://hunter-photo.herokuapp.com/'
+export const API_URL = dev ? import.meta.env['VITE_DEV_URL'] : 'https://hunter-photo.herokuapp.com/';
